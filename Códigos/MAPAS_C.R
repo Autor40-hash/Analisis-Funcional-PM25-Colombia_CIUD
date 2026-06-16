7d3091fbeed5e83c2c7cd730b09bb9748c17cf2b
Autor40 <fbarriossimanca40@correo.unicordoba.edu.co>
2026-06-15 21:21:22 -0500

    first commit

--- Códigos/MAPAS_C.R
+++ Códigos/MAPAS_C.R
@@ -201,8 +201,16 @@ p_cali <- ggplot() +
 p_cali
 
 
-dev.off()
-dev.list()
+
+## Cali
+ggsave("Mapa_Cali_Figura_9.pdf",plot = p_cali,width = 20, height = 12,
+  units = "in")
+
+ggsave("Mapa_Cali_Figura_9.png",plot = p_cali,width = 20,
+  height = 12,units = "in", dpi = 600)
+
+
+
 
 ##=========================================
 ## BOGOTÁ MAPAS 
@@ -361,13 +369,12 @@ p_bogota <- ggplot() +
 
 p_bogota
 
+### Bogotá
+ggsave("Mapa_bogota_Figura_7.pdf",plot = p_bogota,width = 20,
+  height = 12,units = "in")
 
-
-
-
-
-
-
+ggsave( "Mapa_bogota_Figura_7.png", plot = p_bogota,width = 20,
+  height = 12,units = "in", dpi = 600 )
 
 
 
@@ -531,22 +538,192 @@ p_medellin <- ggplot() +
 p_medellin
 
 
+## Medellín
+ggsave( "Mapa_Medellin_Figura_8.pdf", plot = p_medellin,
+  width = 20, height = 9, units = "in")
+
+ggsave( "Mapa_Medellin_Figura_8.png", plot = p_medellin,
+  width = 20, height = 9, units = "in", dpi = 600)
+
 
 
 
 ##=========================================
 ## ÁREA DE ESTUDIO 
 ##=========================================
+ 
+
+Mbogota = loca_sf_filtrado
+Mmedellin = comunas
+Mcali = comunasc
+
+
+maps <- read_excel(
+  "PARTICULAS PM25 BOGOTA MEDELLIN 20231.xlsx",
+  sheet = 3
+)
+
+## -----------------------------------------
+
+ggplot(Mbogota) + geom_sf(fill = "white", color = "black", size = 0.3) +
+  labs(title = "", subtitle = "", caption = "Bogotá") + theme_minimal()
+
+
+ggplot(Mmedellin) + geom_sf(fill = "white", color = "black", size = 0.2) +
+  labs(title = "", caption = "Medellín") + theme_minimal()
+
+
+ggplot(Mcali) + geom_sf(fill = "white", color = "black", size = 0.2) +
+  labs(title = "", caption = "Cali") +theme_minimal()
+
+
+
+maps_sf <- st_as_sf(maps, coords = c("Longitud", "Latitud"),crs = 4326)
+est_bogota <- maps_sf %>% filter(Municipio == "Bogotá") %>% st_transform(st_crs(Mbogota))
+est_medellin <- maps_sf %>% filter(Municipio == "Medellín") %>% st_transform(st_crs(Mmedellin))
+est_cali <- maps_sf %>% filter(Municipio == "Cali") %>% st_transform(st_crs(Mcali))
+
+
+
+## -----------------------------------
+## Bogotá 
+##-------------------------------------
+library(ggspatial)
+coords_bogota <- cbind(est_bogota,st_coordinates(est_bogota))
+coords_bogota$ID <- seq_len(nrow(coords_bogota))
+coords_bogota$Leyenda <- paste0(coords_bogota$ID, "  ", coords_bogota$Estación)
+
+
+p_bogota <- ggplot() +
+  geom_sf(data = Mbogota, fill = "grey92",color = "black",linewidth = 0.2 ) +
+  geom_sf(data = coords_bogota, aes(shape = Leyenda), size = 8,fill = "white",color = "grey9",stroke = 0.6) +
+  geom_sf(data = coords_bogota, aes(shape = Leyenda), size = 5,fill = "grey98",color = "white",stroke = 0.2) +
+  
+  geom_text(data = coords_bogota, aes(X, Y, label = ID),size = 4.5, family = "sans",color = "black") +
+  scale_shape_manual( values = rep(21, nrow(coords_bogota)),name = "       Estaciones") +
+  
+  labs( title = "Bogotá", x = NULL, y = NULL) +
+  guides( shape = guide_legend(override.aes = list(shape = NA),keywidth = 0, keyheight = 0) ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5,face = "bold",size = 20 ),
+    panel.border = element_rect(colour = "black", fill = NA,linewidth = 0.2),
+    panel.grid.major = element_line(colour = "grey90",linewidth = 0.3 ),
+    panel.grid.minor = element_blank(),
+    axis.text = element_text( size = 12,colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    
+    axis.title = element_blank(),
+    legend.position = "right",
+    legend.title = element_text(face = "bold",size = 13 ),
+    legend.text = element_text(size = 13)) +
+  coord_sf() +
+  
+  theme(axis.text.x = element_text(angle = 45,hjust = 1 ) 
+        )
+
+p_bogota
+
+ggsave(filename = "Mapa_Bogota.png",plot = p_bogota,width = 8,
+  height = 6, dpi = 600)
+
+
+
+
+## ----------------------------
+## Medellín
+##------------------------------
+
+coords_medellin <- cbind( est_medellin, st_coordinates(est_medellin))
+coords_medellin$ID <- seq_len(nrow(coords_medellin))
+coords_medellin$Leyenda <- paste0(coords_medellin$ID, "  ", coords_medellin$Estación)
+
+
+
+p_medellin <- ggplot() +
+  
+  geom_sf(data = Mmedellin, fill = "grey92",color = "black",linewidth = 0.2) +
+  geom_sf(data = coords_medellin,  aes(shape = Leyenda),size = 8, fill = "white",
+    color = "grey9",stroke = 0.6) +
+  geom_sf(data = coords_medellin, aes(shape = Leyenda),size = 5,fill = "grey98",
+    color = "white",stroke = 0.2) +
+  geom_text(data = coords_medellin,aes(X, Y, label = ID),size = 4.5,family = "sans",color = "black"  ) +
+  scale_shape_manual(values = rep(21, nrow(coords_medellin)),name = "       Estaciones" ) +
+  
+  labs(title = "Medellín",x = NULL, y = NULL) +
+  guides( shape = guide_legend(override.aes = list(shape = NA),keywidth = 0,
+      keyheight = 0)) +
+  theme_minimal() +
+  theme(
+  plot.title = element_text(hjust = 0.5,face = "bold",size = 20),
+  panel.border = element_rect(colour = "black",fill = NA,linewidth = 0.2),
+  panel.grid.major = element_line(colour = "grey90",linewidth = 0.3),
+  panel.grid.minor = element_blank(),
+  axis.text = element_text(size = 12,colour = "black"),axis.ticks = element_line(colour = "black"),
+  axis.title = element_blank(),legend.position = "right",
+  legend.title = element_text(face = "bold",size = 13),
+  legend.text = element_text(size = 13)) +
+  coord_sf() +
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)
+  )
+
+
+p_medellin
+
+## 
+ggsave(filename = "Mapa_Medellin.png",plot = p_medellin,width = 8,height = 6,dpi = 600)
 
 
 
 
 
 
+## ----------------------------
+## Cali
+##------------------------------
+
+coords_cali <- cbind(est_cali,st_coordinates(est_cali))
+coords_cali$ID <- seq_len(nrow(coords_cali))
+coords_cali$Leyenda <- paste0(coords_cali$ID, "  ", coords_cali$Estación)
 
 
 
 
+p_cali <- ggplot() +
+  
+  geom_sf(data = Mcali,fill = "grey92", color = "black",linewidth = 0.2) +
+  geom_sf(data = coords_cali, aes(shape = Leyenda), size = 8, fill = "white",color = "grey9",stroke = 0.6) +
+  geom_sf(data = coords_cali,  aes(shape = Leyenda),size = 5, fill = "grey98",color = "white",stroke = 0.2) +
+  geom_text(data = coords_cali,  aes(X, Y, label = ID),  size = 4.5,family = "sans",color = "black") +
+  
+  scale_shape_manual( values = rep(21, nrow(coords_cali)),name = "       Estaciones") +
+  
+  labs( title = "Cali", x = NULL,y = NULL) +
+  guides( shape = guide_legend( override.aes = list(shape = NA),keywidth = 0, keyheight = 0 )) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size = 20 ),
+  panel.border = element_rect(colour = "black", fill = NA,linewidth = 0.2),
+  panel.grid.major = element_line(colour = "grey90",linewidth = 0.3),
+  panel.grid.minor = element_blank(),
+    axis.text = element_text(  size = 12, colour = "black"),
+    axis.ticks = element_line(colour = "black"),
+    axis.title = element_blank(),
+    legend.position = "right",
+    legend.title = element_text(face = "bold",size = 13 ),
+    legend.text = element_text(size = 13)) +
+  coord_sf() +
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)
+  )
 
 
+p_cali
+
+ggsave(filename = "Mapa_Cali.png",plot = p_cali,width = 8,height = 6, dpi = 600)
+
+## _------------ MAPAS ------------------##
+
+library(patchwork)
+p_tres <- (p_bogota + p_medellin + p_cali) +plot_layout(ncol = 3)
+ggsave("Mapas_Ciudades_Figura_1.png", plot = p_tres,width = 18,height = 7,dpi = 600)
+ggsave("Mapas_Ciudades_Figura_1.pdf",plot = p_tres,width = 18,height = 7)
 
